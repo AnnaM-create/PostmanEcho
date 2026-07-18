84c3543eb5f070d318e5b349c66109a640b0277c
Annet <foxy2ears@gmail.com>
2026-07-18 22:02:26 +0300

    feat: add initial gradle project and passing rest assured test

---
+++ src/test/java/ru/tests/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+package ru.tests;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    public void shouldEchoPostedData() {
+        RestAssured.baseURI = "https://postman-echo.com";
+
+        String requestData = "some data";
+
+        given()
+                .contentType("text/plain; charset=UTF-8")
+                .body(requestData)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(requestData));
+    }
+}
